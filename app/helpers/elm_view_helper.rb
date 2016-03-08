b0ae7c3c5bb2592966c0fb223f7c139faa0a2a44
Micah Geisel <micah@botandrose.com>
2016-03-07 16:02:48 -0800

    inline javascript into helper so it can work without a view lookup context.

--- app/helpers/elm_view_helper.rb
+++ app/helpers/elm_view_helper.rb
@@ -13,9 +13,37 @@ module ElmViewHelper
   end
 
   def script_tag(module_name, args)
-    content_tag(:script, type: "text/javascript") do
-      render(file: "elm/mount_script.js.erb", locals: { module_name: module_name, args: args })
-    end
+    content_tag(:script, raw(<<-HTML), type: "text/javascript")
+      (function() {
+        var onLoad = function(func) {
+          if (window.attachEvent) {
+            window.attachEvent('onload', yourFunctionName);
+          } else {
+            if (window.onload) {
+              var curronload = window.onload;
+              var newonload = function(evt) {
+                curronload(evt);
+                func(evt);
+              };
+              window.onload = newonload;
+            } else {
+              window.onload = func;
+            }
+          }
+        };
+
+        var currentScript = document.currentScript || (function() {
+          var scripts = document.getElementsByTagName("script");
+          return scripts[scripts.length - 1];
+        })();
+
+        var embedDiv = currentScript.previousSibling;
+
+        onLoad(function() {
+          Elm.embed(#{raw module_name}, embedDiv, #{raw args.to_json});
+        });
+      })();
+    HTML
   end
 
 end
