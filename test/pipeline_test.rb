b5214a805df8e32b9c0fc26c9a4e774a1abf3ac9
Louis Simoneau <simoneau.louis@gmail.com>
2017-01-10 11:04:11 +1100

    Add test against Rails 4.2 application

--- test/pipeline_test.rb
+++ test/pipeline_test.rb
@@ -8,4 +8,12 @@ class TestAssetPipeline < Minitest::Test
       assert system('(cd test/rails5.0 && bundle exec rake assets:precompile && cat public/assets/application-*.js | grep "_elm_lang\$html\$Html\$text(\'Hello, World!\')")'), "Compiling Elm assets in Rails 5 app failed"
     end
   end
+
+  def test_rails_4_2
+    Bundler.with_clean_env do
+      # Run rake assets:precompile in the sample Rails 4.2.7.1 app and confirm that the output JS includes
+      # the compiled Elm
+      assert system('(cd test/rails4.2 && bundle exec rake assets:precompile && cat public/assets/application-*.js | grep "_elm_lang\$html\$Html\$text(\'Hello, World!\')")'), "Compiling Elm assets in Rails 4.2 app failed"
+    end
+  end
 end
