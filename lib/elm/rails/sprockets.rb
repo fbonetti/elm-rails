6cfb5dd0e9f0eb9449c540bd86b66a378c641a6b
Micah Geisel <micah@botandrose.com>
2017-05-29 16:38:45 -0700

    extract DependencyGraph.

--- lib/elm/rails/sprockets.rb
+++ lib/elm/rails/sprockets.rb
@@ -25,7 +25,9 @@ module Elm
 
       def call(input)
         context = input[:environment].context_class.new(input)
-        add_elm_dependencies(input[:filename], input[:load_path], context)
+        DependencyGraph.new(input[:filename], input[:load_path]).each do |dependency|
+          context.depend_on dependency
+        end
         debug = ENV['ELM_RAILS_DEBUG'].nil? ? nil : ENV['ELM_RAILS_DEBUG'].downcase
         debug_flag = case debug
                      when nil, 'false', '0'
@@ -40,29 +42,37 @@ module Elm
 
       private
 
-      # Add all Elm modules imported in the target file as dependencies, then
-      # recursively do the same for each of those dependent modules.
-      def add_elm_dependencies(filename, load_path, context)
-        File.read(filename).each_line.flat_map do |line|
-          # e.g. `import Quiz.QuestionStore exposing (..)`
-          match = line.match(/^import\s+([^\s]+)/)
+      class DependencyGraph < Struct.new(:filename, :load_path)
+        # Add all Elm modules imported in the target file as dependencies, then
+        # recursively do the same for each of those dependent modules.
+        def each &block
+          recurse filename, block
+        end
 
-          next [] if match.nil?
+        private
 
-          # e.g. Quiz.QuestionStore
-          module_name = match.captures[0]
+        def recurse filename, block
+          File.read(filename).each_line do |line|
+            # e.g. `import Quiz.QuestionStore exposing (..)`
+            match = line.match(/^import\s+([^\s]+)/)
 
-          # e.g. Quiz/QuestionStore
-          dependency_logical_name = module_name.gsub(".", "/")
+            next [] if match.nil?
 
-          # e.g. ~/NoRedInk/app/assets/javascripts/Quiz/QuestionStore.elm
-          dependency_filepath = load_path + "/" + dependency_logical_name + ".elm"
+            # e.g. Quiz.QuestionStore
+            module_name = match.captures[0]
 
-          # If we don't find the dependency in our filesystem, assume it's because
-          # it comes in through a third-party package rather than our sources.
-          if File.file? dependency_filepath
-            context.depend_on(dependency_logical_name)
-            add_elm_dependencies(dependency_filepath, load_path, context)
+            # e.g. Quiz/QuestionStore
+            dependency_logical_name = module_name.gsub(".", "/")
+
+            # e.g. ~/NoRedInk/app/assets/javascripts/Quiz/QuestionStore.elm
+            dependency_filepath = load_path + "/" + dependency_logical_name + ".elm"
+
+            # If we don't find the dependency in our filesystem, assume it's because
+            # it comes in through a third-party package rather than our sources.
+            if File.file? dependency_filepath
+              block.call dependency_logical_name
+              recurse dependency_filepath, block
+            end
           end
         end
       end
